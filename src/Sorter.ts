a91ea29c6d1c2bfd77ea69347bbac10dbf0741b6
becouif <lovguya@gmail.com>
2022-07-31 13:53:08 +0300

    update

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,6 +1,12 @@
-import { NumbersCollection } from './NumbersCollection';
+// import { NumbersCollection } from './NumbersCollection';
+
+interface Sortable {
+  length: number;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  swap(leftIndex: number, rightIndex: number): void;
+}
 export class Sorter {
-  constructor(public collection: NumbersCollection /*Todo: fix me later*/) {}
+  constructor(public collection: Sortable /*Todo: fix me later*/) {}
   sort(): void {
     const { length } = this.collection;
 
