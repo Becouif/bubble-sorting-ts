b18d2f0a06337dfc0a6c9d6cdf9ecbbdfe526a1a
becouif <lovguya@gmail.com>
2022-08-04 07:43:35 +0300

    updated

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -3,9 +3,11 @@ class Node {
   constructor(public data: number) {}
 }
 
-export class linkedList {
+// class for linkedList
+export class LinkedList {
   head: Node | null = null;
 
+  // method for adding new Node
   add(data: number): void {
     const node = new Node(data);
 
@@ -19,5 +21,60 @@ export class linkedList {
       tail = tail.next;
     }
     tail.next = node;
+  }
+
+  // method to get length of Node and it is called by .length
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+    return length;
+  }
+  // method to find a node using index
+  at(index: number): Node {
+    if (!this.head) {
+      throw new Error('Index out of bounds');
+    }
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    throw new Error('Index out of bounds');
+  }
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is Empty');
+    }
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex);
+    const rightNode = this.at(rightIndex);
+
+    const leftHand = leftNode.data;
+    leftNode.data = rightNode.data;
+    rightNode.data = leftHand;
+  }
+  print(): void {
+    if (!this.head) {
+      return;
+    }
+    let node: Node | null = this.head;
+    while (node) {
+      console.log(node.data);
+      node = node.next;
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './Linkedlist';
 
 // const numbersCollection = new NumbersCollection([100, 10, 20, 50]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+// console.log('my name is Enoch')
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-5);
+linkedList.add(200);
+linkedList.add(31);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
-console.log('my name is Enoch')
+linkedList.print();
